1a968664655d409d8bc112bd97e9b76004f578d9
20088160 <20088160@mail.wit.ie>
2022-10-12 16:08:17 +0100

    Create Collection

---
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -0,0 +1,22 @@
+package controllers
+
+import models.Note
+
+class NoteAPI {
+    private var notes = ArrayList<Note>()
+
+    fun add(note: Note): Boolean{
+        return notes.add(note)
+    }
+
+    fun listAllNotes(): Any {
+        return if (notes.isEmpty()){
+            "No note is stored"
+        }else{
+            var listofNotes = ""
+            for (i in notes.indices){
+                listofNotes +="${i}:${notes[i]} \n"
+            }
+        }
+    }
+}
